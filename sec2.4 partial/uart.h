4822fc91d196dad7bb4f4c3b12734df1a8e9b597
Matthew R Hofmann <matth2k@eniac.seas.upenn.edu>
2020-02-14 11:39:50 -0500

    removed unfinished code

--- sec2.4 partial/uart.h
+++
@@ -1,7 +0,0 @@
-#define F_CPU 16000000UL
-#define BAUDRATE 9600
-#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
-
-void USART_init(void);
-void USART_send( unsigned char data);
-void USART_putstring(char* StringPtr);
